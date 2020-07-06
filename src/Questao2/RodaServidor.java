84b473dd02b957fe553670b8d3b2616de82ad704
DanielCabral <Daniel-cabral14@hotmail.com>
2020-07-06 19:06:12 -0300

    Questao 3 atualizada

--- src/Questao2/RodaServidor.java
+++ src/Questao2/RodaServidor.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class RodaServidor {
@@ -11,16 +13,17 @@ public class RodaServidor {
 	public static void main(String [] args) throws UnknownHostException, IOException {
 		
 		ServerSocket server = new ServerSocket(12345);
-		String listaDeArquivos = "arquivo1.txt";
+		String listaDeArquivos="src/Questao3/Arquivo.txt";
 	     while(true){
 	       System.out.println("Aguardando conexão...");
 	       Socket con = server.accept();
+	       clientes.add(con);
 	       System.out.println("Cliente conectado...");
-	       Servidor.cont++;
-	       Servidor servidor= new Servidor(con,listaDeArquivos);
-	       Thread t1;
-	       t1 = new Thread(servidor);
-	       t1.start();   
+	       Servidor servidor = new Servidor();
+	       Thread t= new Thread(new Servidor(cliente, listaDeArquivos));
+	       t.start();
+	       
+	    
 	    }
 	}
 	
--- src/Questao3/RodaServidor.java
+++ src/Questao3/RodaServidor.java
@@ -1,41 +1,32 @@
 package Questao3;
 
-import java.awt.List;
+
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.rmi.server.SocketSecurityException;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Scanner;
+
 
 public class RodaServidor {
 	
 	public static void main(String [] args) throws UnknownHostException, IOException {
 		
-		ArrayList<Socket> clientes=new ArrayList<Socket>();
 		ServerSocket server = new ServerSocket(12345);
-	    Servidor cliente1 = null;
+		String listaDeArquivos="src/Questao3/Arquivo.txt";
 	     while(true){
 	       System.out.println("Aguardando conexão...");
 	       Socket con = server.accept();
 	       clientes.add(con);
 	       System.out.println("Cliente conectado...");
-	       Servidor.cont++;
-	       Servidor servidor= new Servidor(con);
-	       Thread t1;
-	       if(Servidor.cont == 2) {
-	    	   cliente1.setCliente2(con);
-	    	   servidor.setCliente2(cliente1.getCliente());
-	    	   t1 = new Thread(servidor);
-	    	   Servidor.cont = 0;
-	       }else {
-	    	   cliente1=servidor;
-	    	   t1 = new Thread(servidor);
-	       }
-	        t1.start();   
+	       Servidor servidor = new Servidor();
+	       Thread t= new Thread(new Servidor(cliente, listaDeArquivos));
+	       t.start();
+	       
+	    
 	    }
 	}
 	
 }
+
